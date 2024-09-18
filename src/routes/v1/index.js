a3aeac493c236b907bf7a313d46d3c8aac585018
tushar-454 <imtushar454@gmail.com>
2024-09-18 23:40:42 +0600

    feat: Add token routes for creating and deleting tokens

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 const usersRoutes = require('./users.js');
 const adminRoutes = require('./admin.js');
 const paymentRoutes = require('./payment.js');
+const tokenRoutes = require('./token.js');
 
 router.use('/api/v1/users', usersRoutes);
 router.use('/api/v1/admin', adminRoutes);
 router.use('/api/v1/payment', paymentRoutes);
+router.use('/api/v1/token', tokenRoutes);
 
 module.exports = router;
